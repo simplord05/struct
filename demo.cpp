289d6c111112239562d566bee4718cdb27ba2139
simplord05 <149597765+simplord05@users.noreply.github.com>
2023-11-14 10:11:57 +0700

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 #define filein() freopen("test.inp", "r", stdin);
 #define fileout() freopen("test.out", "w", stdout);
 using namespace std;
@@ -7,6 +8,7 @@ using namespace std;
 struct sinhVien{
     int stt;
     double T, L, A;
+    double Dtb;
 };
 
 sinhVien a[100];
@@ -14,34 +16,49 @@ int n;
 
 bool cmpT(sinhVien a, sinhVien b)   {return a.T > b.T;}
 
-bool cmpL(sinhVien a, sinhVien b)    {return a.L > b.L;}
+bool cmpL(sinhVien a, sinhVien b)   {return a.L > b.L;}
 
-bool cmpA(sinhVien a, sinhVien b)    {return a.A > b.A;}
+bool cmpA(sinhVien a, sinhVien b)   {return a.A > b.A;}
+
+bool cmpDtb(sinhVien a, sinhVien b)   {return a.Dtb > b.Dtb;}
 
 void sortT(){
     sort(a + 1, a + n + 1, cmpT);
     cout << "       Theo diem mon Toan" << endl;
+    cout << "                      Toan   Ly    Anh" << endl;
     for(int i = 1; i <= n; i++){
         cout << "Sinh vien thu " << a[i].stt << ":";
-        cout << "      " << a[i].T << " " << a[i].L << " " << a[i].A << endl;
+        cout << "      " << a[i].T << "    " << a[i].L << "   " << a[i].A << endl;
     }
 }
 
 void sortL(){
     sort(a + 1, a + n + 1, cmpL);
     cout << "       Theo diem mon Ly" << endl;
+    cout << "                      Toan   Ly    Anh" << endl;
     for(int i = 1; i <= n; i++){
         cout << "Sinh vien thu " << a[i].stt << ":";
-        cout << "      " << a[i].T << " " << a[i].L << " " << a[i].A << endl;
+        cout << "      " << a[i].T << "    " << a[i].L << "   " << a[i].A << endl;
     }
 }
 
 void sortA(){
     sort(a + 1, a + n + 1, cmpA);
     cout << "       Theo diem mon Anh" << endl;
+    cout << "                      Toan   Ly    Anh" << endl;
     for(int i = 1; i <= n; i++){
         cout << "Sinh vien thu " << a[i].stt << ":";
-        cout << "      " << a[i].T << " " << a[i].L << " " << a[i].A << endl;
+        cout << "      " << a[i].T << "    " << a[i].L << "   " << a[i].A << endl;
+    }
+}
+
+void sortDtb(){
+    sort(a + 1, a + n + 1, cmpDtb);
+    cout << "       Theo diem trung binh" << endl;
+    cout << "                      Dtb   Toan   Ly    Anh" << endl;
+    for(int i = 1; i <= n; i++){
+        cout << "Sinh vien thu " << a[i].stt << ":";
+        cout << "      " << a[i].Dtb << "   " << a[i].T << "    " << a[i].L << "   " << a[i].A << endl;
     }
 }
 
@@ -51,9 +68,15 @@ int main(){
     for(int i = 1; i <= n; i++){
         a[i].stt = i;
         cin >> a[i].T >> a[i].L >> a[i].A;
+        a[i].Dtb = (a[i].T + a[i].L + a[i].A) / 3;
     }
+    cout << fixed << setprecision(1); 
     sortT();
+    cout << endl;
     sortL();
+    cout << endl;
     sortA();
+    cout << endl;
+    sortDtb();
     return 0;
 }
